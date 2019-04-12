4052e04f3ed1d0cd12ddf498aec7a031a89b99e1
Volker Benders <Volker.Benders@UundM.com>
2019-04-12 09:48:48 +0200

    reading and writing the chain to form / to disk works, sync for now

--- UuMChain.js
+++ UuMChain.js
@@ -1,16 +1,25 @@
 'use strict'
 const Block = require('./Block')
 const fs = require('fs')
-
+const uumChainFile = 'uumChain.json'
 module.exports = class BlockChain {
+  
+
   constructor(difficulty) {
     var newBlock = new Block('Genesis', '0x01')
     newBlock.calcHash()
     this._chain = [newBlock]
-
+    this.getChainFromFile()
     this.difficulty = difficulty
   }
-
+  /*
+  initChainFromDisk(){
+    var uumChain = fs.readFileSync(uumChainFile);
+    if (uumChain){
+      this.-chain = JSON.parse(uumChain)
+    }
+  }
+  */
   addBlock(data) {
     const lastBlock = this._chain[this._chain.length - 1]
     var block = new Block(data, lastBlock.hash)
@@ -20,9 +29,24 @@ module.exports = class BlockChain {
     this._chain.push(block)
     this.saveChainToFile()
   }
-
+// https://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-file-into-server-memory
+  getChainFromFile(){
+    const uumChainString = fs.readFileSync(uumChainFile, 'utf8')
+    //console.log('getChainFromFile1: uumChainString: ' + JSON.stringify(uumChainString))
+    if (uumChainString){
+      console.log('getChainFromFil2: uumChainString: ' + uumChainString)
+      this._chain = JSON.parse(uumChainString)
+    }else {
+      console.log('getChainFromFile3: uumChainString: ' + JSON.stringify(this._chain))
+    }
+  }
   saveChainToFile(){
-    fs.writeFile('uumChain.json', JSON.stringify(this._chain), 'utf8', function (err) {
+    fs.writeFileSync(uumChainFile, JSON.stringify(this._chain))
+    console.log("The file was saved!");
+  }
+  /*
+  saveChainToFile(){
+    fs.writeFile(uumChainFile, JSON.stringify(this._chain), 'utf8', function (err) {
       if (err) {
         return console.log(err);
       }
@@ -30,4 +54,5 @@ module.exports = class BlockChain {
     console.log("The file was saved!");
     })
   }
+  */
 }
