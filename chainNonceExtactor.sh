13180324a5a070548c61c75fda24fd6f61bb768b
Volker Benders <Volker.Benders@UundM.com>
2019-04-14 14:15:49 +0200

    added timestamp to nonce export

--- chainNonceExtactor.sh
+++ chainNonceExtactor.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Timestamp;Nonce"
+echo "Timestamp;Date;Nonce"
 #cat uumChain.json| jq -r '.[] | (._timestamp|tostring) + " ; " +  (.nonce|tostring)'
 
 # https://stackoverflow.com/questions/3249827/convert-unix-timestamp-to-a-date-string/3249978
 # https://serverfault.com/questions/842322/run-bash-command-with-json-parameters-from-jq
-cat uumChain.json| jq -r '.[] | (._timestamp|tostring) + " ; " +  (.nonce|tostring)'
+cat uumChain.json| jq -r '.[] | (._timestamp|tostring) + " ;  ; " +  (.nonce|tostring)'
 
 
 
